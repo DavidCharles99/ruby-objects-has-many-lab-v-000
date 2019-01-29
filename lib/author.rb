0ef5e87e9b6d7a101e0b8bad041e8effb5d1fa7c
David Charles <ratlover03@gmail.com>
2019-01-29 02:09:42 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,26 +6,26 @@ class Author
    def initialize(name)
     @name = name
     @posts = []
-  end
+   end
 
    def add_post(post)
     self.posts << post
     post.author = self
     @@post_count +=1
-  end
+   end
 
    def add_post_by_title(title)
     post = Post.new(title)
     self.posts << post
     post.author = self
     @@post_count +=1
-  end
-
-   def posts
-    @posts
-  end
+   end
 
    def self.post_count
     @@post_count
-  end
+   end
+
+   def posts
+    @posts
+   end
 end
