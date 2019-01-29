3e0d3c266302313361f7c26b57ccba26a90275dd
David Charles <ratlover03@gmail.com>
2019-01-29 02:06:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,17 +9,15 @@ class Song
     @@all << self
    end
 
-
-
    def self.all
     @@all
    end
+
    def artist_name
     if self.artist
        self.artist.name
     else
       nil
     end
-   end
-
-end
+  end
+ end
